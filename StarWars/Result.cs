fdc8d5d4458042670db48e6719d1dc0e58dbbce6
tanar307@athenepraxis.local <tanar307@athenepraxis.local>
2017-07-03 20:43:09 +0200

    analytics updated with new tasks

--- StarWars/Result.cs
+++ StarWars/Result.cs
@@ -14,10 +14,12 @@ namespace StarWars
                 data.Length > 2 && bool.Parse(data[3])
             );
         }
+
         public DateTime Date { get; }
         public string WarriorName { get; }
         public int WarriorPower { get; }
         public bool GoodWin { get; }
+        public bool IsDraw { get { return WarriorName == null; } }
 
         Result(DateTime date, string warriorName, 
             int warriorPower, bool goodWin)
--- StarWars/ResultAnalytics.cs
+++ StarWars/ResultAnalytics.cs
@@ -15,20 +15,70 @@ namespace StarWars
             
         }
 
+        string MostPowerReamining(bool isGood)
+        {
+            int power = 0;
+            string name = "";
+            foreach (var r in _results)
+            {
+                if (!r.IsDraw && r.WarriorPower > power && r.GoodWin == isGood)
+                {
+                    power = r.WarriorPower;
+                    name = r.WarriorName;
+                }
+            }
+            return name;
+        }
+
+        //string MostPowerRemaingin(bool isGood)
+        //{
+        //    int maxIndex = 0;
+        //    for (int i = 1; i < _results.Length; i++)
+        //    {
+        //        if (!_results[i].IsDraw &&
+        //            _results[i].GoodWin == isGood &&
+        //            _results[maxIndex].WarriorPower < _results[i].WarriorPower)
+        //        {
+        //            maxIndex = i;
+        //        }
+        //    }
+        //    if (maxIndex != 0 || (_results[maxIndex].GoodWin == isGood && !_results[maxIndex].IsDraw))
+        //        return _results[maxIndex].WarriorName;
+
+        //    return null;
+        //    //return maxIndex != 0 || (_results[maxIndex].GoodWin == isGood && !_results[maxIndex].IsDraw) ? 
+        //    //    _results[maxIndex].WarriorName : null);
+        //}
+
+        string MostWinWarriorName(bool isGood)
+        {
+            return null;
+        }
+
+        int SumRemainingPower(bool isGood)
+        {
+            var sum = 0;
+            foreach (var result in _results)
+                if (!result.IsDraw && 
+                    result.GoodWin == isGood)
+                    sum += result.WarriorPower;
+            return sum;
+        }
+
         string MoreWins()
         {
             var goodWin = CountOfWinOfSide(true);
             var darkWin = CountOfWinOfSide(false);
             
-            if (goodWin > darkWin)
-                return "Jók";
-            else if (goodWin < darkWin)
-                return "Rosszak";
-            return "Döntetlen";
-            //return goodWin > badWin ? 
-            //    "Jók" : 
-            //    (goodWin < badWin ? 
-            //        "Rosszak" : "Döntetlen");            
+            return goodWin > darkWin ? 
+                "Jók" : 
+                (goodWin < darkWin ? 
+                "Rosszak" : "Döntetlen");
+            //if (goodWin > darkWin)
+            //    return "Jók";
+            //else if (goodWin < darkWin)
+            //    return "Rosszak";
+            //return "Döntetlen";           
         }
 
         int CountOfWinOfSide(bool isGood)
@@ -46,7 +96,7 @@ namespace StarWars
             {
                 if (result.Date > start && 
                     result.Date < end &&
-                    result.WarriorName != null && 
+                    !result.IsDraw && 
                     result.GoodWin == isGood)
 
                     count++;
